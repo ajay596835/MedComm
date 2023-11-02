b8df57399dd2793ff30ff8313cc4d8f91e5d9a65
ajay596835 <ajay.sharma241099@gmail.com>
2023-11-03 00:58:10 +0530

    intial commit

---
+++ src/main/java/com/medcomm/medcomm/MedcommApplication.java
@@ -0,0 +1,13 @@
+package com.medcomm.medcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedcommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medcomm/medcomm/MedcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medcomm.medcomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
